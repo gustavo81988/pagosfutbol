cce09dce0de26c0cf46523bfcf1ad8ccb0bd306e
Gustavo Ramírez <gustavo81988@gmail.com>
2018-03-15 02:28:54 -0400

    scaffolding padres

---
+++ database/migrations/2018_03_15_061836_create_parents_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateParentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parents', function (Blueprint $table) {
+            $table->integer('ci')->primary();
+            $table->char('name',50);
+            $table->char('lastname',50);
+            $table->char('email',120);
+            $table->integer('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parents');
+    }
+}
