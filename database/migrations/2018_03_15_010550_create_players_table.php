025c1dc76c04b85a1c7c131820c034ca8d646976
Gustavo Ramírez <gustavo81988@gmail.com>
2018-03-14 21:15:52 -0400

    first scaffolding

---
+++ database/migrations/2018_03_15_010550_create_players_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('ci')->unique();
+            $table->integer('parent_id');
+            $table->char('name',50);
+            $table->char('lastname',50);
+            $table->char('nationality',100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
