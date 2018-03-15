247622704bc0d194991204e69ee6a05551d746d0
Gustavo Ramírez <gustavo81988@gmail.com>
2018-03-15 15:43:58 -0400

    frontend asignar representante

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
--- resources/views/parents/create.blade.php
+++ resources/views/parents/create.blade.php
@@ -5,17 +5,66 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Registrar Representante</div>
-
+                <div class="card-header">Asignar Representante</div>
+                
                 <div class="card-body">
-                    <!-- Button trigger modal -->
-                    @include('parents.createmodal')
-                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                      Agregar Representante
-                    </button>
+                    <div class="row">
+                        
+                    <div class="col-md-5">
+                        Cedula representante: <br>
+                        <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+                        <br>
+                        <!-- Button trigger modal -->
+                        @include('parents.createmodal')
+                        <button type="button" name="searchparent" class="btn btn-primary">
+                          Buscar
+                        </button>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                          Nuevo
+                        </button>
+                        
+                    </div>
+                    <div class="col-md-7">
+                        <table class="table table-condensed"> 
+                            <thead> 
+                                <tr> 
+                                    <th>Datos representante</th>
+                                </tr> 
+                            </thead> 
+
+                            <tbody> 
+                                <tr> 
+                                    <th scope="row">Nombre</th>
+                                    <td>Gustavo</td>
+                                </tr> 
+                                <tr> 
+                                    <th scope="row">Apellido</th> 
+                                    <td>Ramirez</td> 
+                                </tr> 
+                                <tr> 
+                                    <th scope="row">Cedula</th> 
+                                    <td colspan="2">18351089</td> 
+                                </tr> 
+                            </tbody> 
+                        </table>
+                        <button type="button" class="btn btn-primary">
+                          Asignar
+                        </button>
+                    </div>
+                    
+                    </div>
+                    
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        
+    });
+</script>
 @endsection
+
+
