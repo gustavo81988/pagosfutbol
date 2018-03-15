9787d9a8c2b590d5f377656469dabe30dced54a5
Gustavo Ramírez <gustavo81988@gmail.com>
2018-03-15 13:40:04 -0400

    indentacion

--- resources/views/parents/create.blade.php
+++ resources/views/parents/create.blade.php
@@ -5,18 +5,15 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                
-                @include('parents.createmodal')
-                
                 <div class="card-header">Registrar Representante</div>
 
                 <div class="card-body">
                     <!-- Button trigger modal -->
+                    @include('parents.createmodal')
                     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
                       Agregar Representante
                     </button>
                 </div>
-                
             </div>
         </div>
     </div>
